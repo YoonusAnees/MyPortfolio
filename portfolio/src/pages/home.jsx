f0ebcd5588c1227d28188863bf11095c0b0e983f
yoonusanees <yoonusaneesniis@gmail.com>
2026-04-21 02:25:16 +0530

    Project Fixed

--- portfolio/src/pages/home.jsx
+++ portfolio/src/pages/home.jsx
@@ -217,7 +217,7 @@ function Home() {
      {
       title: 'Money Tracking System',
       description: 'Tracking System where we can see who sent the money and total amount .',
-      technologies: ['Node.js', 'React', 'Express',  'MongoDB',]
+      technologies: ['Node.js', 'React', 'Express',  'MongoDB'],
       // images: ['MDPOS.png'],
       github: 'https://github.com/YoonusAnees/KBMS-FE.git',
       githubb: 'https://github.com/YoonusAnees/KBMS-BE.git',
